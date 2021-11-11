42c13505e52c5613a08ac985b022bea94460620f
robinsaini226 <56012515+robinsaini226@users.noreply.github.com>
2021-11-10 22:19:42 -0500

    init

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-'use strict';
+"use strict";
+const toggle = document.querySelector(".toggle");
+const label = document.querySelector("label");
+const chckbox = document.querySelector(".chckbox");
+const dollar = document.querySelector(".dollar");
+const year = document.querySelector(".m_y");
+const number = document.querySelector(".number");
+const slider = document.querySelector(".slider");
+const checkmarks = document.querySelector(".features");
+chckbox.addEventListener("click", function () {
+  if (chckbox.checked) {
+    //on state
+    toggle.style.transform = "translate(163%)";
+    label.style.backgroundColor = "var(--strong_cyan)";
+    dollar.textContent = "$16";
+    year.textContent = "/monthly";
+  } else {
+    //off state
+    toggle.style.transform = "translate(0%)";
+    label.style.backgroundColor = "var(--ligh_grayish_blue)";
+    dollar.textContent = "$148";
+    year.textContent = "/yr";
+  }
+});
+
+slider.addEventListener("change", function (e) {
+  number.textContent = `${e.target.value}`;
+});
